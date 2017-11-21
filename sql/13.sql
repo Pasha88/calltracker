66d6638387c13ae863302022ee0230a48dd9da2d
kmorozov <cat_dz@mail.ru>
2017-11-21 17:16:24 +0300

    script number insert to db

---
+++ sql/13.sql
@@ -0,0 +1,15 @@
+CREATE TABLE deploy_scripts (
+  `num` INT NOT NULL,
+  PRIMARY KEY (`num`));
+
+ALTER TABLE deploy_scripts
+ADD COLUMN `exec_date` DATETIME NULL AFTER `num`;
+
+
+ALTER TABLE deploy_scripts
+ADD COLUMN `file_name` VARCHAR(256) NULL AFTER `exec_date`;
+
+ALTER TABLE deploy_scripts
+CHANGE COLUMN `exec_date` `exec_date` DATETIME NOT NULL ;
+
+insert into deploy_scripts values(13, now(), '13.sql');
